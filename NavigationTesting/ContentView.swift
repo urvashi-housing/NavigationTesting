26e71b0f614a26f4b38b42f059c4ca182688ca4e
Urvashi <urvashi.gupta@housing.com>
2023-11-30 12:35:30 +0530

    Initial Commit

---
+++ NavigationTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationTesting
+//
+//  Created by Urvashi Gupta on 30/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationTesting/NavigationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestingApp.swift
+//  NavigationTesting
+//
+//  Created by Urvashi Gupta on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
